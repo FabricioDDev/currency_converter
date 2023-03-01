94c7d8b0335cf4c8aeb063adc5da5182fb004df7
fabricio <fabriciodamiandipaola@gmail.com>
2023-03-01 14:39:32 +0100

    Converter-Builder-Created

--- src/Converter.java
+++ src/Converter.java
@@ -2,7 +2,10 @@
 public class Converter {
 	public Double FirstValue;
 	public Double SecondValue;
-	
+	public Converter(Double FirstValue, Double SecondValue) {
+		this.FirstValue = FirstValue;
+		this.SecondValue = SecondValue;
+	}
 	//comportamientos: convertir, convertir-reversa.
 
 }
