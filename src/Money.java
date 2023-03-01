f4721da1d28ea748c6b1d38afefa369964d35f19
fabricio <fabriciodamiandipaola@gmail.com>
2023-03-01 14:33:21 +0100

    Builder-MoneyClass-Created

--- src/Money.java
+++ src/Money.java
@@ -1,4 +1,25 @@
-
+import java.util.Map;
+import java.util.HashMap;
 public class Money {
 
+	public String Name;
+	private Map<String, Double>Equivalents = new HashMap<String, Double>();
+	
+	public Map<String, Double> getEquivalents() {
+		return Equivalents;
+	}
+	public Double getElementFromEquivalents(String Key) {
+		return Equivalents.get(Key);
+	}
+	public void setEquivalents(String Key,Double value) {
+		Equivalents.put(Key, value);
+	}
+	
+	
+	public Money(String name,double dolar, double euro, double yen) {
+		Name = name;
+		setEquivalents("Dolar", dolar);
+		setEquivalents("Euro", euro);
+		setEquivalents("Yen", yen);
+	}
 }
